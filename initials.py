379b5c2699808df4a06cfa88f01e9cd677a9807c
Chris <72232622+chrisdk95@users.noreply.github.com>
2022-08-09 23:54:42 -0700

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,9 @@
+#Christopher Dike
+#I like Soccer
+print("  CCC    DDDD ")  
+print(" C   C   D   D")  
+print(" C       D   D")  
+print(" C       D   D")  
+print(" C       D   D")  
+print(" C   C   D   D")  
+print("  CCC    DDDD ")  
